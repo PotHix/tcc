58aec595ede064eec20ce6a13887cd59a2028fa3
PotHix <pothix@pothix.com>
2011-12-09 22:00:11 -0200

    Using the local font and adding a jogandoeaprendendo image.

--- slides.js
+++ slides.js
@@ -546,8 +546,9 @@ function addFontStyle() {
   var el = document.createElement('link');
   el.rel = 'stylesheet';
   el.type = 'text/css';
-  el.href = 'http://fonts.googleapis.com/css?family=' +
-            'Open+Sans:regular,semibold,italic,italicsemibold|Droid+Sans+Mono';
+  //el.href = 'http://fonts.googleapis.com/css?family=' +
+  //          'Open+Sans:regular,semibold,italic,italicsemibold|Droid+Sans+Mono';
+  el.href = 'font.css' +
 
   document.body.appendChild(el);
 };
